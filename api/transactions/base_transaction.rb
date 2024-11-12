23284eb351e3caebe5831ae730df79972dce2ed6
Lyna <azerouklyna2001@hotmail.com>
2024-11-12 15:09:32 +0100

    refactor transaction function to take in count the roallback

--- api/transactions/base_transaction.rb
+++ api/transactions/base_transaction.rb
@@ -2,6 +2,13 @@ class BaseTransaction
   include Dry::Transaction
 
   def self.call(*args, &block)
-    new.call(*args, &block)
+    transaction_result = nil
+
+    ActiveRecord::Base.transaction do
+      transaction_result = new.call(*args, &block)
+
+      raise ActiveRecord::Rollback unless transaction_result.failure.nil?
+    end
+    transaction_result
   end
 end
